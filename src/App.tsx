95cb2620ec92d38ec697c33855faad8b9ea94eb4
mike-stumpf <mike.a.stumpf@gmail.com>
2018-07-03 22:34:03 -0500

    added logic to filter by disease type

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 import logo from './logo.svg';
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import { map } from 'lodash';
+import { map, uniq, filter, cloneDeep } from 'lodash';
 import { IMedicationInterface } from './interfaces/types/medication.interface';
 import { IErrorInterface } from './interfaces/responses/error.interface';
 import { IGroupConfigInterface } from './interfaces/types/groupConfig.interface';
@@ -11,9 +11,18 @@ import { IErrorFieldInterface } from './interfaces/responses/errorField.interfac
 interface IState {
     currentSearch: string;
     searchResults: IMedicationInterface[];
+    filteredResults: IMedicationInterface[];
     isInitialPageState: boolean;
     isSearchComplete: boolean;
     error: IErrorInterface | undefined;
+    filterOptions: {
+        formFactors: string[];
+        diseases: string[];
+    };
+    appliedFilters: {
+        formFactors: string[];
+        diseases: string[];
+    };
 }
 
 class App extends React.Component<any, IState> {
@@ -23,25 +32,36 @@ class App extends React.Component<any, IState> {
         this.state = {
             currentSearch: '',
             searchResults: [],
+            filteredResults: [],
             isInitialPageState: true,
             isSearchComplete: false,
-            error: undefined
+            error: undefined,
+            filterOptions: {
+                formFactors: [],
+                diseases: [],
+            },
+            appliedFilters: {
+                formFactors: [],
+                diseases: []
+            }
         };
         this._onSearchChange = this._onSearchChange.bind(this);
         this._submitGroupSearch = this._submitGroupSearch.bind(this);
+        this._addRemoveDiseaseFilter = this._addRemoveDiseaseFilter.bind(this);
+        this._filterSearchResults = this._filterSearchResults.bind(this);
     }
 
     public render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Propeller Health - Front End Developer Skills Assessment</h1>
                 </header>
                 <main className="container-fluid">
                     <div className="row justify-content-center">
                         <div className="col-sm-12 col-md-8 col-lg-6">
-                            <form className="p-3">
+                            <form className="p-3" onSubmit={this._submitGroupSearch}>
                                 <div className="form-group">
                                     <label htmlFor="search-input">Search Groups</label>
                                     <input
@@ -65,8 +85,7 @@ class App extends React.Component<any, IState> {
                                     this.state.currentSearch.length > 0 ?
                                         <button
                                             type="submit"
-                                            className="btn btn-primary"
-                                            onClick={this._submitGroupSearch}>Submit</button>
+                                            className="btn btn-primary">Submit</button>
                                         :
                                         null
                                 }
@@ -76,6 +95,64 @@ class App extends React.Component<any, IState> {
                     {
                         this.state.searchResults.length > 0 ?
                             <div className="row">
+                                {/*filters*/}
+                                <div className="col-12">
+                                    <h3><u>Filters</u></h3>
+                                </div>
+                                <div className="col-4">
+                                    <h5>Form Factor</h5>
+                                    {map(this.state.filterOptions.formFactors, (formFactor: string, index: number) => {
+                                        const id: string = 'form-factor-' + index;
+                                        return (
+                                            <div key={index}>
+                                                <label htmlFor={id}>
+                                                    {formFactor}
+                                                    <input type="checkbox" value="" id={id}/>
+                                                </label>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                                <div className="col-4">
+                                    <h5>Diseases</h5>
+                                    {map(this.state.filterOptions.diseases, (disease: string, index: number) => {
+                                        const id: string = 'disease-' + index;
+                                        return (
+                                            <div key={index}>
+                                                <label htmlFor={id}>
+                                                    {disease}
+                                                    <input
+                                                        type="checkbox"
+                                                        id={id}
+                                                        checked={this.state.appliedFilters.diseases.indexOf(disease) !== -1}
+                                                        onChange={this._addRemoveDiseaseFilter.bind(this, disease)}/>
+                                                </label>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                                <div className="col-4">
+                                    <h5>Sensors</h5>
+                                    <div>
+                                        <label htmlFor="sensor-na">
+                                            No filter
+                                            <input type="radio"
+                                                   name="sensor"
+                                                   id="sensor-na"/>
+                                        </label>
+                                        <br/>
+                                        <label htmlFor="sensor-yes">
+                                            Has sensor
+                                            <input type="radio" name="sensor" id="sensor-yes"/>
+                                        </label>
+                                        <br/>
+                                        <label htmlFor="sensor-no">
+                                            Does not have sensor
+                                            <input type="radio" name="sensor" id="sensor-no"/>
+                                        </label>
+                                    </div>
+                                </div>
+                                {/*data*/}
                                 <div className="col-12">
                                     <table className="table table-striped">
                                         <thead>
@@ -85,11 +162,12 @@ class App extends React.Component<any, IState> {
                                             <th scope="col">Short Name</th>
                                             <th scope="col">Type</th>
                                             <th scope="col">Form Factor</th>
+                                            <th scope="col">Sensors</th>
                                             <th scope="col">Diseases</th>
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        {map(this.state.searchResults, (medication: IMedicationInterface, index: number) => {
+                                        {map(this.state.filteredResults, (medication: IMedicationInterface, index: number) => {
                                             return (
                                                 <tr key={index}>
                                                     <td>{medication.id}</td>
@@ -97,6 +175,7 @@ class App extends React.Component<any, IState> {
                                                     <td>{medication.shortName}</td>
                                                     <td>{medication.type}</td>
                                                     <td>{medication.formFactor}</td>
+                                                    <td>{medication.sensors ? medication.sensors.join(', ') : '-'}</td>
                                                     <td>{medication.diseases.join(', ')}</td>
                                                 </tr>
                                             )
@@ -107,7 +186,7 @@ class App extends React.Component<any, IState> {
                                             </td>
                                             <td className="text-left" colSpan={4}>
                                                 <h5>
-                                                    {this.state.searchResults.length}
+                                                    {this.state.filteredResults.length}
                                                 </h5>
                                             </td>
                                         </tr>
@@ -121,7 +200,7 @@ class App extends React.Component<any, IState> {
                     {
                         !!this.state.error && this.state.isSearchComplete && !this.state.isInitialPageState ?
                             <div className="alert alert-warning" role="alert">
-                                <h4>¡Errors encountered!</h4>
+                                <h4><u>¡Errors encountered!</u></h4>
                                 <div className="row justify-content-center">
                                     {map(this.state.error.elements, (errorField: IErrorFieldInterface, index: number) => {
                                         return (
@@ -151,17 +230,29 @@ class App extends React.Component<any, IState> {
         // reset ui after user input
         this.setState({
             currentSearch: searchValue,
-            searchResults: [],
             isInitialPageState: false,
             isSearchComplete: false,
-            error: undefined
+            error: undefined,
+            // reset search results
+            searchResults: [],
+            filteredResults: [],
+            // reset filters
+            filterOptions: {
+                formFactors: [],
+                diseases: [],
+            },
+            appliedFilters: {
+                formFactors: [],
+                diseases: []
+            }
         });
     }
 
-    private _submitGroupSearch(e: React.MouseEvent<HTMLButtonElement>): void {
+    private _submitGroupSearch(e: React.MouseEvent<HTMLFormElement>): void {
         e.preventDefault();
         // only call api if search string is not empty
         if (this.state.currentSearch.length > 0) {
+            // todo, move api url to .env config file
             axios({
                 method: 'GET',
                 url: 'https://dev.propellerhealth.com/api/groups/' + this.state.currentSearch + '/config',
@@ -170,8 +261,27 @@ class App extends React.Component<any, IState> {
                 }
             })
                 .then((response: AxiosResponse<IGroupConfigInterface>) => {
+                    const uniqueFormFactors: string[] = uniq(
+                        map(response.data.medications, (medication: IMedicationInterface) => {
+                            return medication.formFactor;
+                        })
+                    );
+                    const uniqueDiseases: string[] = [];
+                    map(response.data.medications, (medication: IMedicationInterface) => {
+                        medication.diseases.forEach((disease: string) => {
+                            if (uniqueDiseases.indexOf(disease) === -1) {
+                                uniqueDiseases.push(disease);
+                            }
+                        });
+                    });
+
                     this.setState({
                         searchResults: response.data.medications,
+                        filteredResults: response.data.medications,
+                        filterOptions: {
+                            formFactors: uniqueFormFactors,
+                            diseases: uniqueDiseases,
+                        },
                         isSearchComplete: true
                     });
                 })
@@ -183,6 +293,51 @@ class App extends React.Component<any, IState> {
                     });
                 });
         }
+    }
+
+    private _addRemoveDiseaseFilter(disease: string): void {
+        let appliedDiseaseFilters: string[] = cloneDeep(this.state.appliedFilters.diseases);
+        if (this.state.appliedFilters.diseases.indexOf(disease) === -1) {
+            // add disease to filter
+            appliedDiseaseFilters = [
+                ...appliedDiseaseFilters,
+                disease
+            ]
+        } else {
+            // remove disease from filter
+            appliedDiseaseFilters = filter(this.state.appliedFilters.diseases, disease);
+        }
+        this.setState(
+            {
+                appliedFilters: {
+                    ...this.state.appliedFilters,
+                    diseases: appliedDiseaseFilters
+                }
+            },
+            () => {
+                this._filterSearchResults();
+            });
+    }
+
+    private _filterSearchResults(): void {
+        this.setState({
+            filteredResults: filter(this.state.searchResults, (medication: IMedicationInterface) => {
+                // filter diseases
+                if (this.state.appliedFilters.diseases.length > 0) {
+                    let hasDiseaseFilter: boolean = false;
+                    for (const disease of medication.diseases) {
+                        if (this.state.appliedFilters.diseases.indexOf(disease) !== -1) {
+                            hasDiseaseFilter = true;
+                        }
+                    }
+                    if (!hasDiseaseFilter) {
+                        return false;
+                    }
+                }
+                // filter form factors // todo
+                return true;
+            })
+        });
     }
 }
 
